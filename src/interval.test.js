849171822979e8663de8209532713513a88447d7
Mimyke <mr_bo_yeux_@hotmail.fr>
2019-10-11 00:33:22 +0200

    Test includes

--- src/interval.test.js
+++ src/interval.test.js
@@ -4,7 +4,7 @@ describe('overLaps', function () {
 	var interval = new Interval(2,7);
 	var interval2 = new Interval(8,13);
 	var interval3 = new Interval(3,9);
-	var interval4 = new Interval(1,0);
+
 	test('Test overlaps de [2,7] et [3,9] => true', () => {
     		expect(interval.overlaps(interval3)).toBe(true)
 	});
@@ -17,9 +17,7 @@ describe('overLaps', function () {
 	test('Test overlaps de [8,13] et [2,7] => false', () => {
     		expect(interval2.overlaps(interval)).toBe(false)
 	});
-	test('Test overlaps de [2,7] et [1,0] => throw', () => {
-    		expect(() => { interval.overlaps(interval4)}).toThrow('Wrong interval')
-	});
+	
 });
 
 describe('includes', function () {
